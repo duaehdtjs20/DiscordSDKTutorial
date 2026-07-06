526517ec9e8a105d649af1ecbf0524f3fff4ba6d
YDS <82944267+duaehdtjs20@users.noreply.github.com>
2026-07-06 20:06:26 +0900

    계정 연동 및 권한 부여 구현

---
+++ Assets/DiscordManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Discord.Sdk;
+
+public class DiscordManager : MonoBehaviour
+{
+    [SerializeField] private ulong applicationId;
+
+    private Client client;
+    private string codeVerifier;
+
+    private void Awake()
+    {
+        client = new Client();
+
+        client.AddLogCallback(OnLog, LoggingSeverity.Error);
+        client.SetStatusChangedCallback(OnStatusChanged);
+    }
+    private void OnDestroy()
+    {
+        client.Disconnect();
+    }
+    private void OnLog(string message, LoggingSeverity severity)
+    {
+        Debug.Log($"Log: {severity} - {message}");
+    }
+    private void OnStatusChanged(Client.Status status, Client.Error error, int errorCode)
+    {
+        Debug.Log($"Status changed: {status}");
+
+        if (error != Client.Error.None)
+        {
+            Debug.LogError($"Error: {error}, code: {errorCode}");
+        }
+    }
+    public void StartOAuthFlow()
+    {
+        var authorizationVerifier = client.CreateAuthorizationCodeVerifier();
+        codeVerifier = authorizationVerifier.Verifier();
+
+        var args = new AuthorizationArgs();
+        args.SetClientId(applicationId);
+        args.SetScopes(Client.GetDefaultCommunicationScopes());
+        args.SetCodeChallenge(authorizationVerifier.Challenge());
+        client.Authorize(args, OnAuthorizeResult);
+    }
+    private void OnAuthorizeResult(ClientResult result, string code, string redirectUri)
+    {
+        if (!result.Successful())
+        {
+            Debug.Log($"Authorization result: [{result.Error()}]");
+            return;
+        }
+        GetTokenFromCode(code, redirectUri);
+    }
+    private void GetTokenFromCode(string code, string redirectUri)
+    {
+        client.GetToken(applicationId, code, codeVerifier, redirectUri, OnGetToken);
+    }
+    private void OnGetToken(ClientResult result, string token, string refreshToken, AuthorizationTokenType tokenType, int expiresIn, string scope)
+    {
+        if (token == null || token == string.Empty)
+        {
+            Debug.Log("Failed to retrieve token");
+        }
+        else
+        {
+            client.UpdateToken(AuthorizationTokenType.Bearer, token, OnUpdateToken);
+        }
+    }
+    private void OnUpdateToken(ClientResult result)
+    {
+        if (result.Successful())
+        {
+            client.Connect();
+        }
+        else
+        {
+            Debug.LogError($"Failed to update token: {result.Error()}");
+        }
+    }
+}
